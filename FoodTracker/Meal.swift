f12d40b8ded75d5a0503daf969af5faefdbf8780
Lucas Rydberg <lucasrydberg@Lucass-MacBook-Air.local>
2017-09-04 14:52:24 -0500

    created custom control

---
+++ FoodTracker/Meal.swift
@@ -0,0 +1,8 @@
+//
+//  Meal.swift
+//  FoodTracker
+//
+//  Created by Lucas Rydberg on 9/4/17.
+//  Copyright © 2017 phas3. All rights reserved.
+//
+
--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -16,6 +16,7 @@ class ViewController: UIViewController, UITextFieldDelegate,
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var mealNameLabel: UILabel!
     @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var ratingControl: RatingControl!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -74,10 +75,6 @@ class ViewController: UIViewController, UITextFieldDelegate,
         imagePickerController.delegate = self
         present(imagePickerController, animated: true, completion: nil)
         
-    }
-    
-    @IBAction func setDefaultLabelText(_ sender: UIButton) {
-        mealNameLabel.text = "default"
     }
     
 }
