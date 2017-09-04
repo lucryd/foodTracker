7bebefd71d505bee36d3327721d62dbace2fc9fd
Lucas Rydberg <lucasrydberg@Lucass-MacBook-Air.local>
2017-09-04 09:12:13 -0500

    init commit food tracker

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Lucas Rydberg on 9/1/17.
+//  Copyright © 2017 phas3. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
